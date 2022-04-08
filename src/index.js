ef545c67b4b5fcdf8cd106be58ae78808cab190a
Kartik <86262379+Kartikshettar@users.noreply.github.com>
2022-04-08 18:07:02 +0530

    Delete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import * as ReactDOMClient from 'react-dom/client';
-// Create a root.
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
-
-root.render(<App />);
-
